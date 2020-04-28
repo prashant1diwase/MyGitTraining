18765bf85750c133e25ed72ec373e9929e6b6721
Prashant Diwase <prashant_diwase@persistent.co.in>
2020-04-28 16:46:16 +0530

    Saved Changes

--- File1.java
+++ File1.java
@@ -1,14 +1,15 @@
 
 class Myclass{
-       private void myMethod(){	
+       private return String myMethod(){	
           //new method
+  		return "method";
 	}
         public static void main(){
 		//main method
                 // change new method
                    System.out.println("bug resolved");
-		  MyClass.m();
-
+		  MyClass obj = new MyClass();
+                  System.out.println(obj.myMethod());
 	}
       
 }
