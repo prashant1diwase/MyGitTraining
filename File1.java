00ba3a6ad9ff93521d09152be051eccfa8ae586d
Prashant Diwase <prashant_diwase@persistent.co.in>
2020-04-28 15:45:48 +0530

    Bug: rsolved by adding method

--- File1.java
+++ File1.java
@@ -1,11 +1,14 @@
 
 class Myclass{
-       void myMethod(){	
+       private void myMethod(){	
           //new method
 	}
         public static void main(){
 		//main method
                 // change new method
+                   System.out.println("bug resolved");
+		  MyClass.m();
+
 	}
       
 }
